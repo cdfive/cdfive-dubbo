ac497b60a61096a758eae4c27bba926584386454
xiejihan <176402936@qq.com>
2019-09-17 23:42:30 +0800

    Add MD5 util class for encode function

---
+++ cdfive-common/src/main/java/com/cdfive/common/util/MD5Util.java
@@ -0,0 +1,36 @@
+package com.cdfive.common.util;
+
+import java.security.MessageDigest;
+
+/**
+ * @author cdfive
+ */
+public class MD5Util {
+    public static String encodeByMD5(String str) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(str.getBytes());
+            return byte2string(md.digest());
+        } catch (Exception e) {
+            return str;
+        }
+    }
+
+    public static String byte2string(byte[] b) {
+        StringBuffer hs = new StringBuffer(100);
+        for (int n = 0; n < b.length; n++) {
+            hs.append(byte2fex(b[n]));
+        }
+        return hs.toString();
+    }
+
+    public static String byte2fex(byte ib) {
+        char[] Digit =
+                {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+        char[] ob = new char[2];
+        ob[0] = Digit[(ib >>> 4) & 0X0F];
+        ob[1] = Digit[ib & 0X0F];
+        String s = new String(ob);
+        return s;
+    }
+}
