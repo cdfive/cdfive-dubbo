83400f1f396144a07e007bc6f1ebfffbf3604efb
cdfive <cdfive@qq.com>
2020-02-12 17:53:21 +0800

    Add removeSpecialStr method in StringUtil

--- cdfive-common/src/main/java/com/cdfive/common/util/StringUtil.java
+++ cdfive-common/src/main/java/com/cdfive/common/util/StringUtil.java
@@ -2,12 +2,24 @@ package com.cdfive.common.util;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author cdfive
  */
 public class StringUtil {
 
+    private static final String REGEX_SPECIAL_STR = "[`~!@#$%^&*()+=|{}':;',//[//].<>/?~！@#￥%……&*（）——+|{}【】‘；：”“’。，、？]";
+
+    private static Pattern PATTERN_REGEX_SPECIAL_STR = Pattern.compile(REGEX_SPECIAL_STR);
+
     public static boolean isBlank(String s) {
         return StringUtils.isBlank(s);
+    }
+
+    public static String removeSpecialStr(String str) {
+        Matcher m = PATTERN_REGEX_SPECIAL_STR.matcher(str);
+        return m.replaceAll("").trim();
     }
 }
