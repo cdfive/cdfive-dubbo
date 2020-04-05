261ca7dbeb5237caf77535d1a6d4cea7ceea9538
cdfive <cdfive@qq.com>
2020-04-05 15:14:17 +0800

    Test for static block init via Class.forName

---
+++ cdfive-learn/src/main/java/com/cdfive/learn/javabasic/ClassForNameTest.java
@@ -0,0 +1,13 @@
+package com.cdfive.learn.javabasic;
+
+/**
+ * @author cdfive
+ */
+public class ClassForNameTest {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+//        Class.forName("com.cdfive.learn.javabasic.StaticClass");
+        Class.forName("com.cdfive.learn.javabasic.StaticClass", false, Thread.currentThread().getContextClassLoader());
+        System.out.println("ClassForNameTest done");
+    }
+}
---
+++ cdfive-learn/src/main/java/com/cdfive/learn/javabasic/StaticClass.java
@@ -0,0 +1,10 @@
+package com.cdfive.learn.javabasic;
+
+/**
+ * @author cdfive
+ */
+public class StaticClass {
+    static {
+        System.out.println("StaticClass's static block");
+    }
+}
