1c45b2f8b38be8e9afc4c97e03416a7a92ed4b26
cdfive <cdfive@qq.com>
2020-05-19 16:06:50 +0800

    String#contains throws NPE if argument is null

---
+++ cdfive-learn/src/main/java/com/cdfive/learn/javabasic/StringContainsNPETest.java
@@ -0,0 +1,12 @@
+package com.cdfive.learn.javabasic;
+
+/**
+ * @author cdfive
+ */
+public class StringContainsNPETest {
+
+    public static void main(String[] args) {
+        // NPE
+        System.out.println("123".contains(null));
+    }
+}
