a6e3c06ef7de6ca7d0a12bddc4424639b4257d21
cdfive <cdfive@qq.com>
2020-05-27 16:07:22 +0800

    Test code for Integer length limit overflow

---
+++ cdfive-learn/src/main/java/com/cdfive/learn/javabasic/MaxIntegerTest.java
@@ -0,0 +1,18 @@
+package com.cdfive.learn.javabasic;
+
+/**
+ * @author cdfive
+ */
+public class MaxIntegerTest {
+
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE); // 2147483647
+        System.out.println(Integer.MAX_VALUE + 1); // -2147483648
+
+        System.out.println(Long.MAX_VALUE);// 9223372036854775807
+        System.out.println(Long.MAX_VALUE + 1);// -9223372036854775808
+
+        // java.lang.NumberFormatException: For input string: "21474836478"
+        Integer i = Integer.parseInt("21474836478");
+    }
+}
