6579e6f6b889e7dbc909e654b2029a5cf62f11de
cdfive <cdfive@qq.com>
2020-12-16 13:51:19 +0800

    Add test for GenerateUtil

---
+++ cdfive-common/src/test/java/com/cdfive/com/util/GenerateUtilTest.java
@@ -0,0 +1,38 @@
+package com.cdfive.com.util;
+
+import com.cdfive.common.util.GenerateUtil;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author cdfive
+ */
+public class GenerateUtilTest {
+
+    @Test
+    public void testGenRandomChars() {
+        String s;
+        s = GenerateUtil.genRandomChars(0);
+        System.out.println(s);
+        assertEquals(new String(new char[0]), s);
+
+        s = GenerateUtil.genRandomChars(1);
+        System.out.println(s);
+        assertEquals(1, s.length());
+
+        s = GenerateUtil.genRandomChars(6);
+        System.out.println(s);
+        assertEquals(6, s.length());
+    }
+
+    @Test
+    public void testGenRandomStrId() {
+        System.out.println(GenerateUtil.genRandomStrId());
+        System.out.println(GenerateUtil.genRandomStrId());
+        System.out.println(GenerateUtil.genRandomStrId());
+        System.out.println(GenerateUtil.genRandomStrId());
+        System.out.println(GenerateUtil.genRandomStrId());
+        System.out.println(GenerateUtil.genRandomStrId());
+    }
+}
