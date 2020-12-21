38a6e9116ace47e03d2741da4e00fe7674ea098f
cdfive <cdfive@qq.com>
2020-12-21 11:09:08 +0800

    Add SHA256Util

---
+++ cdfive-common/src/main/java/com/cdfive/common/util/SHA256Util.java
@@ -0,0 +1,28 @@
+package com.cdfive.common.util;
+
+import org.apache.commons.codec.binary.Hex;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * @author cdfive
+ */
+public class SHA256Util {
+
+    public static String encodeBySHA256(String str) {
+        MessageDigest messageDigest;
+        String encdeStr = "";
+        try {
+            messageDigest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = messageDigest.digest(str.getBytes("UTF-8"));
+            encdeStr = Hex.encodeHexString(hash);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return encdeStr;
+    }
+}
