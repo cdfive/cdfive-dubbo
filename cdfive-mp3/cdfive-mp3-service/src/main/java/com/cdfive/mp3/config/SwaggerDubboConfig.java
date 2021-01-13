18627e8b83b796e7ec889411d1eb07dcdd8f11f6
cdfive <cdfive@qq.com>
2021-01-13 10:09:34 +0800

    Disable swagger and swagger-dubbo in production environment

--- cdfive-mp3/cdfive-mp3-service/src/main/java/com/cdfive/mp3/config/SwaggerDubboConfig.java
+++ cdfive-mp3/cdfive-mp3-service/src/main/java/com/cdfive/mp3/config/SwaggerDubboConfig.java
@@ -1,49 +1,25 @@
 package com.cdfive.mp3.config;
 
 import com.deepoove.swagger.dubbo.annotations.EnableDubboSwagger;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.DocumentationCache;
-import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger.web.InMemorySwaggerResourcesProvider;
 import springfox.documentation.swagger.web.SwaggerResource;
 import springfox.documentation.swagger.web.SwaggerResourcesProvider;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 
 /**
  * @author cdfive
  */
+@ConditionalOnProperty(name = "swagger.dubbo.enable", matchIfMissing = false)
 @EnableDubboSwagger
-@EnableSwagger2
 @Configuration
-public class Swagger2Config {
-
-    @Bean
-    public Docket docket() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.cdfive.mp3.controller"))
-                .paths(PathSelectors.any())
-                .build();
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("mp3-service")
-                .description("mp3-service description")
-                .version("0.0.1")
-                .build();
-    }
+public class SwaggerDubboConfig {
 
     @Primary
     @Bean
