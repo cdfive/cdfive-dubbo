9c705151cd99466164502217585d9563ebb483b4
cdfive <cdfive@qq.com>
2021-03-01 09:31:09 +0800

    Add test for order of different map

---
+++ cdfive-learn/src/main/java/com/cdfive/learn/collection/DifferentMapTest.java
@@ -0,0 +1,32 @@
+package com.cdfive.learn.collection;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * @author cdfive
+ */
+public class DifferentMapTest {
+
+    public static void main(String[] args) {
+        testMap(new HashMap<>());
+        testMap(new TreeMap<>());
+        testMap(new LinkedHashMap<>());
+    }
+
+    public static void testMap(Map<Integer, String> map) {
+        System.out.println(StringUtils.center(map.getClass().getName(), 30, "-"));
+        map.put(2, "a");
+        map.put(3, "b");
+        map.put(1, "c");
+        map.put(12, "qq");
+        map.put(18, "ww");
+        map.put(15, "ee");
+        System.out.println(map.values());
+        System.out.println(map.keySet());
+    }
+}
