3c87071f7e93eb0edb0a7356036199ff19dd8fb8
cdfive <cdfive@qq.com>
2021-03-17 13:42:55 +0800

    Add double accuracy test

---
+++ cdfive-learn/src/main/java/com/cdfive/learn/javabasic/DoubleTest.java
@@ -0,0 +1,23 @@
+package com.cdfive.learn.javabasic;
+
+/**
+ * @author cdfive
+ */
+public class DoubleTest {
+
+    public static void main(String[] args) {
+        // 0.30000000000000004
+        System.out.println(0.1 * 3);;// 0.30000000000000004
+
+        double d = 0.1;
+        System.out.println(d * 3);// 0.30000000000000004
+        System.out.println(d * 0.3);// 0.03
+
+        System.out.println((int) d);// 0
+        System.out.println((int) (d * 3));// 0
+        System.out.println((int) (d * 9));// 0
+        System.out.println((int) (d * 11));// 1
+        System.out.println((int) (d * 0.5));// 0
+        System.out.println((int) d * 0.5);// 0.0
+    }
+}
