9670f9d9391a58381795d47108e025dca23a33d8
cdfive <cdfive@qq.com>
2021-05-12 17:12:13 +0800

    Add test for String#split

--- cdfive-learn/src/main/java/com/cdfive/learn/javabasic/StringSplitTest.java
+++ cdfive-learn/src/main/java/com/cdfive/learn/javabasic/StringSplitTest.java
@@ -1,5 +1,7 @@
 package com.cdfive.learn.javabasic;
 
+import java.util.Arrays;
+
 /**
  * @author cdfive
  */
@@ -20,5 +22,20 @@ public class StringSplitTest {
         System.out.println("1,2,,".split(",", 0).length);
         // 1
         System.out.println("1".split(",", 2).length);
+
+        // [1]
+        System.out.println(Arrays.asList("1-".split("-")));
+        // [1, ]
+        System.out.println(Arrays.asList("1-".split("-", -1)));
+        // [1, ]
+        System.out.println(Arrays.asList("1-".split("-", 2)));
+        // [1, 2-3]
+        System.out.println(Arrays.asList("1-2-3".split("-", 2)));
+        // [1, 2-]
+        System.out.println(Arrays.asList("1-2-".split("-", 2)));
+        // [1, 2, ]
+        System.out.println(Arrays.asList("1-2-".split("-", 3)));
+        // [1, 2, ]
+        System.out.println(Arrays.asList("1-2-".split("-", -1)));
     }
 }
