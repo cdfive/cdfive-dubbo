05a66cb65fd28b557ede0c05529345c88070069e
cdfive <cdfive@qq.com>
2021-05-21 10:40:53 +0800

    Controller抽象类增加protected日志变量

--- cdfive-common/src/main/java/com/cdfive/common/base/AbstractController.java
+++ cdfive-common/src/main/java/com/cdfive/common/base/AbstractController.java
@@ -1,11 +1,15 @@
 package com.cdfive.common.base;
 
 import com.cdfive.common.api.ApiResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author cdfive
  */
 public class AbstractController {
+
+    protected Logger log = LoggerFactory.getLogger(getClass());
 
     protected <T> ApiResponse<T> succ() {
         return ApiResponse.succ();
