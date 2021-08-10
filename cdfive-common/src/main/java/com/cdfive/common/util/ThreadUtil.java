768eb7e5a2b56f8db8518c0365700de1e8dbb4c0
cdfive <cdfive@qq.com>
2021-08-10 21:51:39 +0800

    Add ThreadUtil and newFixedBlockingThreadPool util functions

---
+++ cdfive-common/src/main/java/com/cdfive/common/util/ThreadUtil.java
@@ -0,0 +1,41 @@
+package com.cdfive.common.util;
+
+import org.springframework.scheduling.concurrent.CustomizableThreadFactory;
+
+import java.util.concurrent.*;
+
+/**
+ * @author cdfive
+ */
+public class ThreadUtil {
+
+    public static ExecutorService newFixedBlockingThreadPool(int nThreads) {
+        return new ThreadPoolExecutor(nThreads, nThreads, 0, TimeUnit.SECONDS, new SynchronousQueue<Runnable>(), new RejectedExecutionHandler() {
+            @Override
+            public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+                try {
+                    executor.getQueue().put(r);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+    }
+
+    public static ExecutorService newFixedBlockingThreadPool(int nThreads, ThreadFactory threadFactory) {
+        return new ThreadPoolExecutor(nThreads, nThreads, 0, TimeUnit.SECONDS, new SynchronousQueue<Runnable>(), threadFactory, new RejectedExecutionHandler() {
+            @Override
+            public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+                try {
+                    executor.getQueue().put(r);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+    }
+
+    public static ExecutorService newFixedBlockingThreadPool(int nThreads, String threadNamePrefix) {
+        return newFixedBlockingThreadPool(nThreads, new CustomizableThreadFactory(threadNamePrefix));
+    }
+}
