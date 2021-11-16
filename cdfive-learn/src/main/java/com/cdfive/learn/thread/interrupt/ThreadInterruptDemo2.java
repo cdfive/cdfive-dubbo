89a20ae4d195a723bd9fe3772f3627ae25c269b8
cdfive <cdfive@qq.com>
2021-11-16 10:33:33 +0800

    Improve demo for thread interrupt

--- cdfive-learn/src/main/java/com/cdfive/learn/thread/interrupt/ThreadInterruptDemo2.java
+++ cdfive-learn/src/main/java/com/cdfive/learn/thread/interrupt/ThreadInterruptDemo2.java
@@ -3,14 +3,17 @@ package com.cdfive.learn.thread.interrupt;
 /**
  * @author cdfive
  */
-public class ThreadInterruptDemo {
+public class ThreadInterruptDemo2 {
 
     public static void main(String[] args) throws Exception {
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
                 for (; ; ) {
-
+                    if (Thread.currentThread().isInterrupted()) {
+                        // if thread is interrupted, break loop and the thread exit
+                        break;
+                    }
                 }
             }
         });
@@ -20,9 +23,9 @@ public class ThreadInterruptDemo {
         System.out.println(thread.isInterrupted()); // true
         System.out.println(thread.interrupted()); // false currentThread is main
         System.out.println(Thread.interrupted()); // false currentThread is main
-        System.out.println(thread.isInterrupted()); // true
+        System.out.println(thread.isInterrupted()); // false, the interrupt state has been reset
 
         thread.join();
-        System.out.println("main done"); // will not execute since the thread(in running state) is not really interrupted
+        System.out.println("main thread done"); // will be executed
     }
 }
