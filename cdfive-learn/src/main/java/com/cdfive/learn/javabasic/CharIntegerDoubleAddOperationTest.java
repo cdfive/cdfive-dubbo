6a9935fc12e9ce3a33bcaab999879bc97929abfd
cdfive <cdfive@qq.com>
2021-12-02 13:32:33 +0800

    Add test demo for add operation with Char,Integer,Double

---
+++ cdfive-learn/src/main/java/com/cdfive/learn/javabasic/CharIntegerDoubleAddOperationTest.java
@@ -0,0 +1,21 @@
+package com.cdfive.learn.javabasic;
+
+/**
+ * @author cdfive
+ */
+public class CharIntegerDoubleAddOperationTest {
+
+    public static void main(String[] args) {
+        // 98
+        System.out.println('a' + 1);
+
+        // 98.618
+        System.out.println('a' + 1 + 0.618);
+
+        // a1
+        System.out.println("a" + 1);
+
+        //a10.618
+        System.out.println("a" + 1 + 0.618);
+    }
+}
