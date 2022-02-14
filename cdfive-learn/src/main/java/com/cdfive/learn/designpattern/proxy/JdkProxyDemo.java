a2e2991eac4e73dee7dc4ff421b3e77fad2b1259
cdfive <cdfive@qq.com>
2022-02-14 15:07:56 +0800

    Separate test demo for InvocationHandler

--- cdfive-learn/src/main/java/com/cdfive/learn/designpattern/proxy/JdkProxyDemo.java
+++ cdfive-learn/src/main/java/com/cdfive/learn/designpattern/proxy/JdkProxyDemo.java
@@ -1,5 +1,7 @@
 package com.cdfive.learn.designpattern.proxy;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
@@ -25,7 +27,22 @@ public class JdkProxyDemo implements InvocationHandler {
     }
 
     public static void main(String[] args) {
+        test1();
+
+        System.out.println(StringUtils.center("分隔线", 32, "#"));
+
+        test2();
+    }
+
+    public static void test1() {
         UserServiceImpl userServiceImpl = new UserServiceImpl();
+        JdkProxyDemo jdkProxyDemo = new JdkProxyDemo();
+        UserService userService = (UserService) jdkProxyDemo.bind(userServiceImpl);
+        userService.addUser("cdfive");
+    }
+
+    public static void test2() {
+        UserService userServiceImpl = new UserServiceImpl();
         JdkProxyDemo jdkProxyDemo = new JdkProxyDemo();
         UserService userService = (UserService) jdkProxyDemo.bind(userServiceImpl);
         userService.addUser("cdfive");
