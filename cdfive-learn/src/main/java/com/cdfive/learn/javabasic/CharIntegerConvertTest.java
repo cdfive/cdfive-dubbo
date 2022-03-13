751769bfca41697b23173b48aa5a81a794bb360c
cdfive <cdfive@qq.com>
2022-03-13 11:36:45 +0800

    Add test demo for convert from Character to Integer

---
+++ cdfive-learn/src/main/java/com/cdfive/learn/javabasic/CharIntegerConvertTest.java
@@ -0,0 +1,39 @@
+package com.cdfive.learn.javabasic;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @author cdfive
+ */
+public class CharIntegerConvertTest {
+
+    public static void main(String[] args) {
+        String str = "abc123";
+
+        char c1 = str.charAt(0);
+        // a
+        System.out.println(c1);
+        // 95
+        System.out.println((int) c1);
+        // 10
+        System.out.println(Character.getNumericValue(c1));
+        // java.lang.NumberFormatException
+        try {
+            System.out.println(Integer.valueOf(String.valueOf(c1)));
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(StringUtils.center("分隔符", 30, "-"));
+
+        char c2 = str.charAt(3);
+        // 1
+        System.out.println(c2);
+        // 49
+        System.out.println((int) c2);
+        // 1
+        System.out.println(Character.getNumericValue(c2));
+        // 1
+        System.out.println(Integer.valueOf(String.valueOf(c2)));
+    }
+}
