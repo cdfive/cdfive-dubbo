9af557d82db2776b9d0fc870eeebafacbe0d887c
cdfive <cdfive@qq.com>
2022-06-04 08:07:00 +0800

    Add rest api for get thread pool info of tomcat

---
+++ cdfive-common/src/main/java/com/cdfive/common/monitor/TomcatMonitorController.java
@@ -0,0 +1,28 @@
+package com.cdfive.common.monitor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.embedded.tomcat.TomcatWebServer;
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author cdfive
+ */
+@Slf4j
+@RequestMapping("/monitor/tomcat")
+@RestController
+public class TomcatMonitorController {
+
+    @Autowired
+    private ServletWebServerApplicationContext applicationContext;
+
+    @RequestMapping("/threadPool")
+    public String threadPool() {
+        TomcatWebServer webServer = (TomcatWebServer) applicationContext.getWebServer();
+        String info = webServer.getTomcat().getConnector().getProtocolHandler().getExecutor().toString();
+        log.info("TomcatMonitorController threadPool=>" + info);
+        return info;
+    }
+}
