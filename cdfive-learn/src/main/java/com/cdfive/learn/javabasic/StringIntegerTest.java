26cb25f759afa4aba09c85cff6c1f5b544bdf437
cdfive <cdfive@qq.com>
2022-06-08 19:49:23 +0800

    Add test demo String and Integer

---
+++ cdfive-learn/src/main/java/com/cdfive/learn/javabasic/StringIntegerTest.java
@@ -0,0 +1,12 @@
+package com.cdfive.learn.javabasic;
+
+/**
+ * @author cdfive
+ */
+public class StringIntegerTest {
+
+    public static void main(String[] args) {
+        // 53
+        System.out.println("5" + 3);
+    }
+}
