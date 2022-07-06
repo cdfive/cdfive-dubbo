40c7dbd1af2bfe1055e3ebad2ed9488d19b04e0f
cdfive <cdfive@qq.com>
2022-07-06 10:17:17 +0800

    Refactor class name for AsyncTaskExecutor and improve comment

--- cdfive-common/src/main/java/com/cdfive/common/util/LimitedAsyncTaskExecutor.java
+++ cdfive-common/src/main/java/com/cdfive/common/util/LimitedAsyncTaskExecutor.java
@@ -12,22 +12,28 @@ import java.util.Iterator;
 import java.util.concurrent.CountDownLatch;
 
 /**
- * 异步任务执行器
+ * 受限的异步任务执行器
+ * <p>
+ * 1. 任务本身需是异步执行的
+ * 2. 此执行器控制异步执行并发数, 避免大量并发导致不可控
  *
- * @author cdfive
+ * @author xiejihan
+ * @date 2022-05-18
  */
 @Slf4j
-public class AsyncTaskExecutor {
+public class LimitedAsyncTaskExecutor {
 
+    // 并发数
     private int concurrency;
 
+    // 跟踪id
     private String traceId;
 
-    public AsyncTaskExecutor(int concurrency) {
+    public LimitedAsyncTaskExecutor(int concurrency) {
         this(concurrency, CommonUtil.getTraceId());
     }
 
-    public AsyncTaskExecutor(int concurrency, String traceId) {
+    public LimitedAsyncTaskExecutor(int concurrency, String traceId) {
         Assert.isTrue(concurrency > 0, "concurrency must be greater than 0");
         this.concurrency = concurrency;
         this.traceId = traceId;
@@ -43,8 +49,9 @@ public class AsyncTaskExecutor {
         Assert.isTrue(total > 0, "total must be greater than 0");
         Assert.notNull(taskExecutor, "taskExecutor can't be null");
 
-        log.info(traceId + ",AsyncTaskExecutor executeTasks start,total={},concurrency={}", total, concurrency);
+        log.info(traceId + ",LimitedAsyncTaskExecutor executeTasks start,total={},concurrency={}", total, concurrency);
         long totalStart = System.currentTimeMillis();
+        long batchStart = System.currentTimeMillis();
 
         CountDownLatch latch = null;
         Runnable callback = null;
@@ -55,6 +62,7 @@ public class AsyncTaskExecutor {
             T task = tasks.next();
             index++;
             if (latch == null) {
+                batchStart = System.currentTimeMillis();
                 batchIndex++;
                 latch = new CountDownLatch((batchIndex < batchTotal) ? concurrency : (total - index + 1));
                 CountDownLatch finalLatch = latch;
@@ -67,21 +75,24 @@ public class AsyncTaskExecutor {
             }
 
             Context context = new Context(index, total, batchIndex, batchTotal);
+            log.info(traceId + ",LimitedAsyncTaskExecutor executeTask start,concurrency={},context={}", concurrency, context);
             taskExecutor.executeTask(task, callback, context);
 
             if (index % concurrency == 0 || index == total) {
                 try {
                     latch.await();
                 } catch (InterruptedException e) {
-                    log.error(traceId + ",AsyncTaskExecutor await error", e);
+                    log.error(traceId + ",LimitedAsyncTaskExecutor await error", e);
                 }
 
                 latch = null;
                 callback = null;
+                log.info(traceId + ",LimitedAsyncTaskExecutor batch done,cost={}ms,index=({}/{}),batch=({}/{})"
+                        , (System.currentTimeMillis() - batchStart), index, total, batchIndex, batchTotal);
             }
         }
 
-        log.info(traceId + ",AsyncTaskExecutor executeTasks success,total cost={}ms,concurrency={}", (System.currentTimeMillis() - totalStart), concurrency);
+        log.info(traceId + ",LimitedAsyncTaskExecutor executeTasks success,total cost={}ms,concurrency={}", (System.currentTimeMillis() - totalStart), concurrency);
     }
 
     public int getConcurrency() {
@@ -92,11 +103,20 @@ public class AsyncTaskExecutor {
         return traceId;
     }
 
+    /**
+     * 任务执行器
+     */
     public static interface TaskExecutor<T> {
 
+        /**
+         * 执行任务,任务本身需是异步执行的
+         */
         void executeTask(T task, Runnable callback, Context context);
     }
 
+    /**
+     * 上下文
+     */
     @NoArgsConstructor
     @AllArgsConstructor
     @Data
@@ -104,12 +124,16 @@ public class AsyncTaskExecutor {
 
         private static final long serialVersionUID = 2916376548269524746L;
 
+        // 下标
         private int index;
 
+        // 总数
         private int total;
 
+        // 批量下标
         private int batchIndex;
 
+        // 批量总数
         private int batchTotal;
     }
 }
