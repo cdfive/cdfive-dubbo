3aa6930fa1055fa8584bccbc43a7231d333fa17f
cdfive <cdfive@qq.com>
2022-12-06 14:29:58 +0800

    Configure Nacos through bootstrap.yml and move application.yml to Nacos for configuration

--- cdfive-mp3/cdfive-mp3-service/src/main/java/com/cdfive/mp3/Mp3Application.java
+++ cdfive-mp3/cdfive-mp3-service/src/main/java/com/cdfive/mp3/Mp3Application.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EurekaClientAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -15,7 +16,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ImportResource({"classpath:/config/applicationContext.xml"})
 @EnableCaching
 @EnableJpaRepositories(repositoryFactoryBeanClass = BaseRepositoryFactoryBean.class, basePackages = "com.cdfive.mp3")
-@SpringBootApplication(scanBasePackages = {"com.cdfive"})
+@SpringBootApplication(scanBasePackages = {"com.cdfive"}, exclude = {EurekaClientAutoConfiguration.class})
 public class Mp3Application {
     public static void main(String[] args) {
         SpringApplication.run(Mp3Application.class, args);
