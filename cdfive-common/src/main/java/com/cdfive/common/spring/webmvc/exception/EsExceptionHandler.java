7d018e60e2506618fd75dcb29eeed624c4ac1afa
cdfive <cdfive@qq.com>
2023-01-02 15:15:25 +0800

    Improve unified exception handling, handle EsException

---
+++ cdfive-common/src/main/java/com/cdfive/common/spring/webmvc/exception/EsExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.cdfive.common.spring.webmvc.exception;
+
+import com.cdfive.es.exception.EsException;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author cdfive
+ */
+@Component
+public class EsExceptionHandler extends AbstractExceptionHandler<EsException> {
+    @Override
+    public ModelAndView handleException(HttpServletRequest request, HttpServletResponse response, Object handler, EsException ex) {
+        ModelAndView mav = new ModelAndView(new MappingJackson2JsonView());
+
+        mav.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+
+        mav.addObject("msg", ex.getMessage());
+
+        mav.addObject("ts", System.currentTimeMillis());
+        return mav;
+    }
+}
