67ff47d2c0c652982169aa59deac06751b33bd55
cdfive <cdfive@qq.com>
2023-01-31 09:19:44 +0800

    More test demo of Integer

--- cdfive-learn/src/main/java/com/cdfive/learn/javabasic/IntegerTest.java
+++ cdfive-learn/src/main/java/com/cdfive/learn/javabasic/IntegerTest.java
@@ -14,8 +14,11 @@ public class IntegerTest {
         a = 12315;
         System.out.println(a / 100.0);// 123.15
 
-        System.out.println(Integer.MIN_VALUE);
-        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE); // -2147483648
+        System.out.println(Integer.MAX_VALUE); // 2147483647
+
+        System.out.println(Integer.MAX_VALUE + 1);
+        System.out.println((Integer.MAX_VALUE + 1) == Integer.MIN_VALUE);
 
 //        Integer i = 11456725200; // Compile error: Integer number too large
         // Exception in thread "main" java.lang.NumberFormatException: For input string: "11456725200"
@@ -27,5 +30,13 @@ public class IntegerTest {
 //        at java.lang.Integer.parseInt(Integer.java:542)
 //        at java.lang.Integer.valueOf(Integer.java:766)
 //        System.out.println(Integer.valueOf(null));
+
+        // Infinite loop
+        /*
+        int i = 2147483646;
+        while (true) {
+            System.out.println(i++);
+        }
+        */
     }
 }
