e4371fd74fa29c91394e229731a1df4fa4701998
cdfive <cdfive@qq.com>
2023-02-23 18:01:38 +0800

    Optimize definition of SongController by implementing SongApi

--- cdfive-mp3/cdfive-mp3-service/src/main/java/com/cdfive/mp3/controller/SongController.java
+++ cdfive-mp3/cdfive-mp3-service/src/main/java/com/cdfive/mp3/controller/SongController.java
@@ -1,10 +1,10 @@
 package com.cdfive.mp3.controller;
 
+import com.cdfive.mp3.api.SongApi;
 import com.cdfive.mp3.service.SongService;
 import com.cdfive.mp3.vo.song.FindAllSongRespVo;
 import com.cdfive.mp3.vo.song.SongListVo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,26 +12,25 @@ import java.util.List;
 /**
  * @author cdfive
  */
-@RequestMapping("/mp3")
 @RestController
-public class SongController {
+public class SongController implements SongApi {
 
     @Autowired
     private SongService songService;
 
-    @RequestMapping("/all")
+    @Override
     public FindAllSongRespVo findAllSong() {
         FindAllSongRespVo respVo = songService.findAllSong();
         return respVo;
     }
 
-    @RequestMapping("/random_list")
+    @Override
     public List<SongListVo> findRandomSongList(Integer num) {
         List<SongListVo> respVos = songService.findRandomSongList(num);
         return respVos;
     }
 
-    @RequestMapping("/play")
+    @Override
     public Integer play(Integer id, String ip) {
         Integer respVo = songService.play(id, ip);
         return respVo;
