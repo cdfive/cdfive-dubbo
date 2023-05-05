d883fb9ea50f6f8a72e88a923a2fb13eb0fc09b1
cdfive <cdfive@qq.com>
2023-05-05 23:00:38 +0800

    More test case for using stream#concat

--- cdfive-learn/src/main/java/com/cdfive/learn/stream/StreamConcatTest.java
+++ cdfive-learn/src/main/java/com/cdfive/learn/stream/StreamConcatTest.java
@@ -3,8 +3,10 @@ package com.cdfive.learn.stream;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -32,14 +34,37 @@ public class StreamConcatTest {
                 .map(o -> o.getId()).distinct().collect(Collectors.toList());
         // [1, 2, 3, 4, 5]
         System.out.println(ids);
+
+        System.out.println(StringUtils.center("分隔符", 50, "-"));
+
+        List<Item> list = new ArrayList<Item>() {{
+            add(Item.of(1, "aa", "aa"));
+            add(Item.of(2, "aa", "dd"));
+            add(Item.of(2, "bb", "cc"));
+            add(Item.of(2, "ee", null));
+            add(Item.of(2, null, "ff"));
+            add(Item.of(2, "gg", "aa"));
+        }};
+
+        List<String> names = Stream.concat(list.stream().filter(o -> o.getName() != null).map(o -> o.getName())
+                , list.stream().filter(o -> o.getNickName() != null).map(o -> o.getNickName())).distinct().collect(Collectors.toList());
+        // [aa, bb, ee, gg, dd, cc, ff]
+        System.out.println(names);
     }
 
     @NoArgsConstructor
     @AllArgsConstructor(staticName = "of")
     @Data
     public static class Item {
+
+        public static Item of(Integer id, String name) {
+            return new Item(id, name, null);
+        }
+
         private Integer id;
 
         private String name;
+
+        private String nickName;
     }
 }
