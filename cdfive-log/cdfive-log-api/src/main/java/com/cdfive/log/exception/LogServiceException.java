6ebc0c7b17fd90b8b696448e02d160f6f2939e3b
cdfive <cdfive@qq.com>
2023-05-12 09:55:39 +0800

    Refactor LogServiceException using cdfive-framework module

--- cdfive-log/cdfive-log-api/src/main/java/com/cdfive/log/exception/LogServiceException.java
+++ cdfive-log/cdfive-log-api/src/main/java/com/cdfive/log/exception/LogServiceException.java
@@ -1,24 +1,34 @@
 package com.cdfive.log.exception;
 
-import com.cdfive.common.exception.ServiceException;
+import com.cdfive.framework.exception.ServiceException;
 
 /**
  * @author cdfive
  */
 public class LogServiceException extends ServiceException {
 
+    private static final long serialVersionUID = 5495179260580460835L;
+
     public LogServiceException() {
+    }
+
+    public LogServiceException(Integer code) {
+        super(code);
     }
 
     public LogServiceException(String message) {
         super(message);
     }
 
-    public LogServiceException(String message, Throwable cause) {
-        super(message, cause);
+    public LogServiceException(Integer code, String message) {
+        super(code, message);
     }
 
-    public LogServiceException(Throwable cause) {
-        super(cause);
+    public LogServiceException(Integer code, String message, Throwable cause) {
+        super(code, message, cause);
+    }
+
+    public LogServiceException(Integer code, Throwable cause) {
+        super(code, cause);
     }
 }
--- cdfive-log/cdfive-log-service/src/main/java/com/cdfive/log/service/AbstractLogService.java
+++ cdfive-log/cdfive-log-service/src/main/java/com/cdfive/log/service/AbstractLogService.java
@@ -1,14 +1,14 @@
 package com.cdfive.log.service;
 
-import com.cdfive.common.base.AbstractService;
-import com.cdfive.common.exception.ServiceException;
+import com.cdfive.framework.base.AbstractService;
+import com.cdfive.framework.exception.ServiceException;
 import com.cdfive.log.exception.LogServiceException;
 
 /**
  * @author cdfive
  */
 public class AbstractLogService extends AbstractService {
-    
+
     @Override
     protected ServiceException exception(String msg) {
         return new LogServiceException(msg);
