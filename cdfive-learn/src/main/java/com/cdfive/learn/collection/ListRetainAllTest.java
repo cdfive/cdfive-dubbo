fd20602d33aea5ea8f3e2d11dd1dcc8031953749
cdfive <cdfive@qq.com>
2023-05-16 22:45:41 +0800

    Add test demo of using List#retainAll

---
+++ cdfive-learn/src/main/java/com/cdfive/learn/collection/ListRetainAllTest.java
@@ -0,0 +1,35 @@
+package com.cdfive.learn.collection;
+
+import com.google.common.collect.Lists;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author cdfive
+ */
+public class ListRetainAllTest {
+
+    public static void main(String[] args) {
+        List<Integer> list = Lists.newArrayList(1, 2, 3);
+        // [1, 2, 3]
+        System.out.println(list);
+        boolean result = list.retainAll(Lists.newArrayList(1, 2));
+        // true
+        System.out.println(result);
+        // [1, 2] !!Note that the data of original list has been changed
+        System.out.println(list);
+
+        System.out.println(StringUtils.center("分隔线", 50, "-"));
+
+        List<Integer> list2 = Lists.newArrayList(1, 2, 3);
+        // [1, 2, 3]
+        System.out.println(list2);
+        // [1, 2]
+        System.out.println(CollectionUtils.intersection(list2, Lists.newArrayList(1, 2)));
+        // [1, 2, 3] !!Note that the data of original list has no change
+        System.out.println(list2);
+    }
+}
