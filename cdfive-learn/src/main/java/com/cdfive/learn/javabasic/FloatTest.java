082fe1bb65d6517fbbac628e728391a2b889f0b4
cdfive <cdfive@qq.com>
2023-06-08 09:29:07 +0800

    More test of Float

--- cdfive-learn/src/main/java/com/cdfive/learn/javabasic/FloatTest.java
+++ cdfive-learn/src/main/java/com/cdfive/learn/javabasic/FloatTest.java
@@ -1,5 +1,7 @@
 package com.cdfive.learn.javabasic;
 
+import java.math.BigDecimal;
+
 /**
  * @author cdfive
  */
@@ -33,5 +35,17 @@ public class FloatTest {
 
         // NaN
         System.out.println(Double.NaN);
+
+        // 3.4028235E38
+        System.out.println(Float.MAX_VALUE);
+
+        // 1.4E-45
+        System.out.println(Float.MIN_VALUE);
+
+        // 340282346638528859811704183484516925440
+        System.out.println(new BigDecimal(Float.MAX_VALUE));
+
+        // 1.40129846432481707092372958328991613128026194187651577175706828388979108268586060148663818836212158203125E-45
+        System.out.println(new BigDecimal(Float.MIN_VALUE));
     }
 }
