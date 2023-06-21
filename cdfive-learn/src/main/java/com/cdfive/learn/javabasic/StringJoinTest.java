530aada0c5a0930ba2e3113602a333bca36368a4
cdfive <cdfive@qq.com>
2023-06-21 17:10:10 +0800

    Add test demo for String#join method

---
+++ cdfive-learn/src/main/java/com/cdfive/learn/javabasic/StringJoinTest.java
@@ -0,0 +1,16 @@
+package com.cdfive.learn.javabasic;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * @author cdfive
+ */
+public class StringJoinTest {
+
+    public static void main(String[] args) {
+        System.out.println(String.join(",", Arrays.asList("a", "b", "c")));
+        System.out.println(String.join(",", Arrays.asList("")));
+        System.out.println(String.join(",", Collections.emptyList()));
+    }
+}
