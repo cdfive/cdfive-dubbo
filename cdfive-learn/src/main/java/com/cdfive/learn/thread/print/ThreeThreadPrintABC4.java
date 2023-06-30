6f4df778989ba38ca6d1583ae66fd4ef4cfd3b68
cdfive <cdfive@qq.com>
2023-06-30 14:18:31 +0800

    Improve demo of print with thread

--- cdfive-learn/src/main/java/com/cdfive/learn/thread/print/ThreeThreadPrintABC4.java
+++ cdfive-learn/src/main/java/com/cdfive/learn/thread/print/ThreeThreadPrintABC4.java
@@ -1,6 +1,7 @@
 package com.cdfive.learn.thread.print;
 
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Semaphore;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -14,14 +15,16 @@ public class ThreeThreadPrintABC4 {
 
     public static void main(String[] args) throws Exception {
         int max = 10;
-        int num = 3;
+        char keyStart = 'A';
+        int keySize = 3;
+        CountDownLatch latch = new CountDownLatch(keySize);
 
         // Keys to print
-        List<String> keys = IntStream.range('A', 'A' + num).mapToObj(i -> String.valueOf((char) i)).collect(Collectors.toList());
+        List<String> keys = IntStream.range(keyStart, keyStart + keySize).mapToObj(i -> String.valueOf((char) i)).collect(Collectors.toList());
 
         // Init thread, including key, max and acquireSemaphore
         List<MyThread> threads = IntStream.range(0, keys.size())
-                .mapToObj(i -> new MyThread(keys.get(i), max, new Semaphore(i == 0 ? 1 : 0)))
+                .mapToObj(i -> new MyThread(keys.get(i), max, latch, new Semaphore(i == 0 ? 1 : 0)))
                 .collect(Collectors.toList());
 
         // Init thread's name and releaseSemaphore
@@ -32,6 +35,9 @@ public class ThreeThreadPrintABC4 {
 
         // Start all threads
         threads.forEach(t -> t.start());
+
+        latch.await();
+        System.out.println("done");
     }
 
     public static class MyThread extends Thread {
@@ -43,6 +49,8 @@ public class ThreeThreadPrintABC4 {
         private Semaphore acquireSemaphore;
 
         private Semaphore releaseSemaphore;
+
+        private CountDownLatch latch;
 
         public MyThread() {
 
@@ -59,6 +67,13 @@ public class ThreeThreadPrintABC4 {
             this.acquireSemaphore = acquireSemaphore;
         }
 
+        public MyThread(String key, Integer max, CountDownLatch latch, Semaphore acquireSemaphore) {
+            this.key = key;
+            this.max = max;
+            this.acquireSemaphore = acquireSemaphore;
+            this.latch = latch;
+        }
+
         public MyThread(String key, Integer max, Semaphore acquireSemaphore, Semaphore releaseSemaphore) {
             this.key = key;
             this.max = max;
@@ -73,6 +88,7 @@ public class ThreeThreadPrintABC4 {
                 System.out.println(Thread.currentThread().getName() + "=>" + key + ",times=" + (i + 1));
                 releaseSemaphore.release();
             }
+            latch.countDown();
         }
 
         public String getKey() {
