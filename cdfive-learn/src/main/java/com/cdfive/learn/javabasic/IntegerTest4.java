775278e368f832cb1274bca782d31c08ddacf2fb
cdfive <cdfive@qq.com>
2023-07-24 22:29:41 +0800

    Add test demo of Integer

---
+++ cdfive-learn/src/main/java/com/cdfive/learn/javabasic/IntegerTest4.java
@@ -0,0 +1,16 @@
+package com.cdfive.learn.javabasic;
+
+/**
+ * @author cdfive
+ * @date 2023-07-19
+ */
+public class IntegerTest4 {
+
+    public static void main(String[] args) {
+        System.out.println(1 << 3); // 8
+        System.out.println(1 << 2); // 4
+        System.out.println(1 << 1); // 2
+        System.out.println(1 << 0); // 1
+        System.out.println(1 << -1); // -2147483648
+    }
+}
