10c11acbb00354181621e0b0cbe2d7219abab7b0
cdfive <cdfive@qq.com>
2023-07-27 09:41:03 +0800

    Improve test demo for List#remove

--- cdfive-learn/src/main/java/com/cdfive/learn/javabasic/ListRemoveTest.java
+++ cdfive-learn/src/main/java/com/cdfive/learn/javabasic/ListRemoveTest.java
@@ -1,5 +1,7 @@
 package com.cdfive.learn.javabasic;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -10,10 +12,61 @@ import java.util.List;
 public class ListRemoveTest {
 
     public static void main(String[] args) {
+        try {
+            remove1();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(StringUtils.center("分隔线", 50, "-"));
+
         remove2();
+
+        System.out.println(StringUtils.center("分隔线", 50, "-"));
+
+        remove3();
+
+        System.out.println(StringUtils.center("分隔线", 50, "-"));
+
+        remove4();
+
+        System.out.println(StringUtils.center("分隔线", 50, "-"));
+
+        remove5();
     }
 
     public static void remove1() {
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        System.out.println(list);
+
+        int size = list.size();
+        for (int i = 0; i < size; i++) {
+            String item = list.get(i);
+            list.remove(item);
+        }
+
+        System.out.println(list);
+    }
+
+    public static void remove2() {
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        System.out.println(list);
+
+        for (int i = 0; i < list.size(); i++) {
+            String item = list.get(i);
+            list.remove(item);
+        }
+
+        System.out.println(list);
+    }
+
+    public static void remove3() {
         List<String> list = new ArrayList<>();
         list.add("1");
         list.add("2");
@@ -28,7 +81,7 @@ public class ListRemoveTest {
         System.out.println(list);
     }
 
-    public static void remove2() {
+    public static void remove4() {
         List<String> list = new ArrayList<>();
         list.add("1");
         list.add("2");
@@ -44,7 +97,7 @@ public class ListRemoveTest {
         System.out.println(list);
     }
 
-    public static void remove3() {
+    public static void remove5() {
         List<String> list = new ArrayList<>();
         list.add("1");
         list.add("2");
