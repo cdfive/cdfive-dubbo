38e9adc99b6dbe373d30b7ce6f5268d8b2c8a146
cdfive <cdfive@qq.com>
2023-09-08 09:14:16 +0800

    More test case using Calendar and LocalDate

--- cdfive-learn/src/main/java/com/cdfive/learn/javabasic/DateTest.java
+++ cdfive-learn/src/main/java/com/cdfive/learn/javabasic/DateTest.java
@@ -3,6 +3,9 @@ package com.cdfive.learn.javabasic;
 import org.apache.commons.lang3.time.DateFormatUtils;
 import org.apache.commons.lang3.time.DateUtils;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -19,5 +22,17 @@ public class DateTest {
 
         // 1970-01-01 08:00:00.000
         System.out.println(DateFormatUtils.format(date, "yyyy-MM-dd HH:mm:ss.SSS"));
+
+        // 2023-09-05
+        DayOfWeek dayOfWeek = LocalDate.now().getDayOfWeek();
+        // TUESDAY
+        System.out.println(dayOfWeek);
+        // 2
+        System.out.println(dayOfWeek.getValue());
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        // 3
+        System.out.println(calendar.get(Calendar.DAY_OF_WEEK));
     }
 }
