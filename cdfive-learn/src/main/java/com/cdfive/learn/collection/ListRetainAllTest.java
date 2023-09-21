93d65e9aeedf51e430458a1c2e7802b74e1ba1ec
cdfive <cdfive@qq.com>
2023-09-21 18:12:57 +0800

    More test case using List#retainAll

--- cdfive-learn/src/main/java/com/cdfive/learn/collection/ListRetainAllTest.java
+++ cdfive-learn/src/main/java/com/cdfive/learn/collection/ListRetainAllTest.java
@@ -4,7 +4,6 @@ import com.google.common.collect.Lists;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,6 +20,14 @@ public class ListRetainAllTest {
         System.out.println(result);
         // [1, 2] !!Note that the data of original list has been changed
         System.out.println(list);
+        // false
+        System.out.println(Lists.newArrayList(1, 2).retainAll(list));
+        // false
+        System.out.println(Lists.newArrayList().retainAll(list));
+        // true
+        System.out.println(Lists.newArrayList(1, 2, 3, 4).retainAll(list));
+        // true
+        System.out.println(Lists.newArrayList(1, 4).retainAll(list));
 
         System.out.println(StringUtils.center("分隔线", 50, "-"));
 
