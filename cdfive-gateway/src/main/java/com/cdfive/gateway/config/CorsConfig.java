a6212582518f2add2d0d1cabc197d61c3255742c
cdfive <cdfive@qq.com>
2023-10-07 10:27:21 +0800

    Comment CorsConfig class in mp3 and gateway module

--- cdfive-gateway/src/main/java/com/cdfive/gateway/config/CorsConfig.java
+++ cdfive-gateway/src/main/java/com/cdfive/gateway/config/CorsConfig.java
@@ -1,48 +1,53 @@
-package com.cdfive.gateway.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.web.cors.reactive.CorsUtils;
-import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.WebFilter;
-import org.springframework.web.server.WebFilterChain;
-import reactor.core.publisher.Mono;
-
-/**
- * @author cdfive
- */
-@Configuration
-public class CorsConfig {
-
-    @Bean
-    public WebFilter corsFilter() {
-        return (ServerWebExchange ctx, WebFilterChain chain) -> {
-            ServerHttpRequest request = ctx.getRequest();
-            if (!CorsUtils.isCorsRequest(request)) {
-                return chain.filter(ctx);
-            }
-            HttpHeaders requestHeaders = request.getHeaders();
-            ServerHttpResponse response = ctx.getResponse();
-            HttpMethod requestMethod = requestHeaders.getAccessControlRequestMethod();
-            HttpHeaders headers = response.getHeaders();
-            headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, requestHeaders.getOrigin());
-            headers.addAll(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, requestHeaders.getAccessControlRequestHeaders());
-            if (requestMethod != null) {
-                headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, requestMethod.name());
-            }
-            headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
-            headers.add(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, "*");
-//            headers.add(HttpHeaders.ACCESS_CONTROL_MAX_AGE, MAX_AGE);
-            if (request.getMethod() == HttpMethod.OPTIONS) {
-                response.setStatusCode(HttpStatus.OK);
-                return Mono.empty();
-            }
-            return chain.filter(ctx);
-        };
-    }
-}
+//package com.cdfive.gateway.config;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.server.reactive.ServerHttpRequest;
+//import org.springframework.http.server.reactive.ServerHttpResponse;
+//import org.springframework.web.cors.reactive.CorsUtils;
+//import org.springframework.web.server.ServerWebExchange;
+//import org.springframework.web.server.WebFilter;
+//import org.springframework.web.server.WebFilterChain;
+//import reactor.core.publisher.Mono;
+//
+///**
+// * @author cdfive
+// */
+//@Slf4j
+//@Configuration
+//public class CorsConfig {
+//
+//    @Bean
+//    public WebFilter corsFilter() {
+//        return (ServerWebExchange ctx, WebFilterChain chain) -> {
+//            ServerHttpRequest request = ctx.getRequest();
+//            if (!CorsUtils.isCorsRequest(request)) {
+//                return chain.filter(ctx);
+//            }
+//            HttpHeaders requestHeaders = request.getHeaders();
+//            ServerHttpResponse response = ctx.getResponse();
+//            HttpMethod requestMethod = requestHeaders.getAccessControlRequestMethod();
+//            HttpHeaders headers = response.getHeaders();
+//
+//            log.info("debug={}", requestHeaders.getOrigin());
+//            headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, requestHeaders.getOrigin());
+////            headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+//            headers.addAll(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, requestHeaders.getAccessControlRequestHeaders());
+//            if (requestMethod != null) {
+//                headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, requestMethod.name());
+//            }
+//            headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+//            headers.add(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, "*");
+////            headers.add(HttpHeaders.ACCESS_CONTROL_MAX_AGE, MAX_AGE);
+//            if (request.getMethod() == HttpMethod.OPTIONS) {
+//                response.setStatusCode(HttpStatus.OK);
+//                return Mono.empty();
+//            }
+//            return chain.filter(ctx);
+//        };
+//    }
+//}
