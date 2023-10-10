1da035f1244092f1d32c5c96c0cc2f9e596d59a7
cdfive <cdfive@qq.com>
2023-10-10 16:37:06 +0800

    More test case for the usage of String#replace and String#replaceAll

--- cdfive-learn/src/main/java/com/cdfive/learn/javabasic/StringReplaceTest.java
+++ cdfive-learn/src/main/java/com/cdfive/learn/javabasic/StringReplaceTest.java
@@ -6,21 +6,31 @@ package com.cdfive.learn.javabasic;
 public class StringReplaceTest {
 
     public static void main(String[] args) {
-        String name = "123.ab.c";
+        String name = "123.ab.c3";
         String result;
 
-        // !!!Pay attention to that it replaces all
+        // Case1: !!!Pay attention to that it replaces all
         result = name.replace(".","。");
-        // 123。ab。c
+        // 123。ab。c3
         System.out.println(result);
 
-        // !!!Pay attention to the first argument, it's a regex expression
+        // Case2: Since name has no \. and replace nothing
+        result = name.replace("\\.","。");
+        // 123.ab.c3
+        System.out.println(result);
+
+        // Case3: !!!Pay attention to the first argument, it's a regex expression
         result = name.replaceAll(".","。");
-        // 。。。。。。。。
+        // 。。。。。。。。。
         System.out.println(result);
 
-        // 123。ab。c
+        // Case4: !!!Pay attention to it's equivalent to Case1
         result = name.replaceAll("\\.","。");
+        // 123。ab。c3
+        System.out.println(result);
+
+        // Case5: 12。.ab.c。
+        result = name.replaceAll("3","。");
         System.out.println(result);
     }
 }
