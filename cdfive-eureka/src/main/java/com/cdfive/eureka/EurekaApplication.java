56f5133a55195ef80b606e41fd3703776f06c472
cdfive <cdfive@qq.com>
2023-10-13 13:35:04 +0800

    Add InMemoryHttpTraceRepository Bean to make HttpTraceEndpoint available

--- cdfive-eureka/src/main/java/com/cdfive/eureka/EurekaApplication.java
+++ cdfive-eureka/src/main/java/com/cdfive/eureka/EurekaApplication.java
@@ -2,8 +2,11 @@ package com.cdfive.eureka;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author cdfive
@@ -16,5 +19,10 @@ public class EurekaApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class);
         log.info("cdfive-eureka application started!");
+    }
+
+    @Bean
+    public HttpTraceRepository httpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
     }
 }
