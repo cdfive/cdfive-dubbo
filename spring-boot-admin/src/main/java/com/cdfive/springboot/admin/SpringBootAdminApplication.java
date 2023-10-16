a7f1b0990e87f1c47f348170e594707ff441a4fa
cdfive <cdfive@qq.com>
2023-10-16 09:16:23 +0800

    Update the version of spring-boot-admin dependency to 2.3.0 and do a simple test

--- spring-boot-admin/src/main/java/com/cdfive/springboot/admin/SpringBootAdminApplication.java
+++ spring-boot-admin/src/main/java/com/cdfive/springboot/admin/SpringBootAdminApplication.java
@@ -3,6 +3,7 @@ package com.cdfive.springboot.admin;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @EnableAutoConfiguration
+@EnableDiscoveryClient
 @EnableAdminServer
 public class SpringBootAdminApplication {
 
