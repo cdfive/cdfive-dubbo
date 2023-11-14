60ec5938632a74954b80492b32d9ed987378aa44
cdfive <cdfive@qq.com>
2023-11-14 09:07:07 +0800

    Test demo of String#join

--- cdfive-learn/src/main/java/com/cdfive/learn/guava/JoinerTest.java
+++ cdfive-learn/src/main/java/com/cdfive/learn/guava/JoinerTest.java
@@ -2,6 +2,9 @@ package com.cdfive.learn.guava;
 
 import com.google.common.base.Joiner;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 /**
  * @author cdfive
  */
@@ -11,8 +14,12 @@ public class JoinerTest {
         Object[] arguments = new Object[2];
         arguments[0] = null;
         arguments[1] = "a";
-//        String str = Joiner.on(",").join(arguments);// NPE
-        String str = Joiner.on(",").skipNulls().join(arguments);// OK
+
+//        String str = Joiner.on(",").join(arguments); // NPE
+        String str = Joiner.on(",").skipNulls().join(arguments); // OK
         System.out.println(str);
+
+        String str2 = String.join(",", Arrays.asList(arguments).stream().map(o -> (String) o).collect(Collectors.toList()));
+        System.out.println(str2); // null, a
     }
 }
