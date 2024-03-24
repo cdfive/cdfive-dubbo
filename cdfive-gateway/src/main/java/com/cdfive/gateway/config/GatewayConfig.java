e3a5b20b739b9050bce49e42de7a5832f513f27e
cdfive <cdfive@qq.com>
2024-03-24 22:16:59 +0800

    Add custom ErrorWebExceptionHandler of spring-cloud-gateway

--- cdfive-gateway/src/main/java/com/cdfive/gateway/config/GatewayConfig.java
+++ cdfive-gateway/src/main/java/com/cdfive/gateway/config/GatewayConfig.java
@@ -1,19 +1,48 @@
 package com.cdfive.gateway.config;
 
-import com.cdfive.gateway.predicate.CustomReadBodyRoutePredicateFactory;
+import com.cdfive.gateway.exception.CustomErrorWebExceptionHandler;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ResourceProperties;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.reactive.error.ErrorAttributes;
+import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.result.view.ViewResolver;
+
+import java.util.stream.Collectors;
 
 /**
  * @author cdfive
  */
+@EnableConfigurationProperties({ServerProperties.class})
 @Configuration
 public class GatewayConfig {
 
-//    @Primary
+    @Autowired
+    private ServerProperties serverProperties;
+
     @Bean
-    public CustomReadBodyRoutePredicateFactory customReadBodyRoutePredicateFactory() {
-        return new CustomReadBodyRoutePredicateFactory();
+    @Order(-1)
+    public ErrorWebExceptionHandler errorWebExceptionHandler(ErrorAttributes errorAttributes,
+                                                             ResourceProperties resourceProperties, ObjectProvider<ViewResolver> viewResolvers,
+                                                             ServerCodecConfigurer serverCodecConfigurer, ApplicationContext applicationContext) {
+        CustomErrorWebExceptionHandler exceptionHandler = new CustomErrorWebExceptionHandler(errorAttributes,
+                resourceProperties, this.serverProperties.getError(), applicationContext);
+        exceptionHandler.setViewResolvers(viewResolvers.orderedStream().collect(Collectors.toList()));
+        exceptionHandler.setMessageWriters(serverCodecConfigurer.getWriters());
+        exceptionHandler.setMessageReaders(serverCodecConfigurer.getReaders());
+        return exceptionHandler;
     }
+
+//    @Primary
+//    @Bean
+//    public CustomReadBodyRoutePredicateFactory customReadBodyRoutePredicateFactory() {
+//        return new CustomReadBodyRoutePredicateFactory();
+//    }
 }
---
+++ cdfive-gateway/src/main/java/com/cdfive/gateway/exception/CustomErrorWebExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.cdfive.gateway.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.web.ErrorProperties;
+import org.springframework.boot.autoconfigure.web.ResourceProperties;
+import org.springframework.boot.autoconfigure.web.reactive.error.DefaultErrorWebExceptionHandler;
+import org.springframework.boot.web.reactive.error.ErrorAttributes;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+/**
+ * @author cdfive
+ */
+@Slf4j
+public class CustomErrorWebExceptionHandler extends DefaultErrorWebExceptionHandler {
+
+    public CustomErrorWebExceptionHandler(ErrorAttributes errorAttributes, ResourceProperties resourceProperties, ErrorProperties errorProperties, ApplicationContext applicationContext) {
+        super(errorAttributes, resourceProperties, errorProperties, applicationContext);
+    }
+
+    @Override
+    protected void logError(ServerRequest request, ServerResponse response, Throwable throwable) {
+        log.error(request.exchange().getLogPrefix() + formatError(throwable, request), throwable);
+    }
+
+    private String formatError(Throwable ex, ServerRequest request) {
+        String reason = ex.getClass().getSimpleName() + ": " + ex.getMessage();
+        return "Resolved [" + reason + "] for HTTP " + request.methodName() + " " + request.path();
+    }
+}
