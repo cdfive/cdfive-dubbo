fe2c7e1f0f5fb6462bf8353bac5db2469ae6596c
cdfive <cdfive@qq.com>
2024-07-24 09:10:36 +0800

    Improve comment

--- cdfive-demo/cdfive-demo-mybatis/src/main/java/com/cdfive/demo/mybatis/ext/ExtWebMvcConfigurationSupport.java
+++ cdfive-demo/cdfive-demo-mybatis/src/main/java/com/cdfive/demo/mybatis/ext/ExtWebMvcConfigurationSupport.java
@@ -1,10 +1,14 @@
 package com.cdfive.demo.mybatis.ext;
 
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
 /**
+ * Note: not recommend!
+ * {@link WebMvcAutoConfiguration} will not take effect
+ *
  * @author cdfive
  */
 @Configuration
