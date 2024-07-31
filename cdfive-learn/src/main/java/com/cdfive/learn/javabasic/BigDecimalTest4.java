b05987050c15b2fb95cced79cf9162f077e1f42d
cdfive <cdfive@qq.com>
2024-07-31 09:09:18 +0800

    Add test demo of using BigDecimal

--- cdfive-learn/src/main/java/com/cdfive/learn/javabasic/BigDecimalTest4.java
+++ cdfive-learn/src/main/java/com/cdfive/learn/javabasic/BigDecimalTest4.java
@@ -1,5 +1,7 @@
 package com.cdfive.learn.javabasic;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.math.BigDecimal;
 
 /**
@@ -8,8 +10,45 @@ import java.math.BigDecimal;
 public class BigDecimalTest4 {
 
     public static void main(String[] args) {
+        case1();
+
+        System.out.println(StringUtils.center("分隔线", 50, "-"));
+
+        case2();
+
+        System.out.println(StringUtils.center("分隔线", 50, "-"));
+
+        case3();
+    }
+
+    private static void case1() {
         // java.lang.NumberFormatException
-        BigDecimal val = new BigDecimal("a");
-        System.out.println(val);
+        try {
+            BigDecimal val = new BigDecimal("a");
+            System.out.println(val);
+        } catch (Exception e) {
+            System.out.println(e.getClass().getName());
+        }
+    }
+    private static void case2() {
+        // java.lang.NullPointerException
+        try {
+            Double d = null;
+            BigDecimal val = new BigDecimal(d);// unboxing
+            System.out.println(val);
+        } catch (Exception e) {
+            System.out.println(e.getClass().getName());
+        }
+    }
+
+    private static void case3() {
+        // java.lang.NumberFormatException
+        try {
+            double d = 0 / 0d; // NaN
+            BigDecimal val = new BigDecimal(d);
+            System.out.println(val);
+        } catch (Exception e) {
+            System.out.println(e.getClass().getName());
+        }
     }
 }
