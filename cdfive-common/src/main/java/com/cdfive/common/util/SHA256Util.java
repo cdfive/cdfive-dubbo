456d1b0fb739d938b51431a73169a9bd72865d88
cdfive <cdfive@qq.com>
2024-08-20 10:54:55 +0800

    Improve SHA256Util

--- cdfive-common/src/main/java/com/cdfive/common/util/SHA256Util.java
+++ cdfive-common/src/main/java/com/cdfive/common/util/SHA256Util.java
@@ -1,5 +1,6 @@
 package com.cdfive.common.util;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.binary.Hex;
 
 import java.io.UnsupportedEncodingException;
@@ -9,20 +10,23 @@ import java.security.NoSuchAlgorithmException;
 /**
  * @author cdfive
  */
+@Slf4j
 public class SHA256Util {
 
     public static String encodeBySHA256(String str) {
-        MessageDigest messageDigest;
-        String encdeStr = "";
         try {
-            messageDigest = MessageDigest.getInstance("SHA-256");
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
             byte[] hash = messageDigest.digest(str.getBytes("UTF-8"));
-            encdeStr = Hex.encodeHexString(hash);
+            return Hex.encodeHexString(hash);
         } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
+            log.error("encodeBySHA256 NoSuchAlgorithmException error,str={}", str, e);
+            throw new RuntimeException("encodeBySHA256 error", e);
         } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+            log.error("encodeBySHA256 UnsupportedEncodingException error,str={}", str, e);
+            throw new RuntimeException("encodeBySHA256 error", e);
+        } catch (Exception e) {
+            log.error("encodeBySHA256 error,str={}", str, e);
+            throw new RuntimeException("encodeBySHA256 error", e);
         }
-        return encdeStr;
     }
 }
