98c239090c47f250a4b769fe174e9d67fb8b530f
cdfive <cdfive@qq.com>
2024-08-25 22:25:30 +0800

    Dev cdfive-eureka module

--- cdfive-common/src/main/java/com/cdfive/common/util/SHA256Util.java
+++ cdfive-common/src/main/java/com/cdfive/common/util/SHA256Util.java
@@ -1,9 +1,13 @@
 package com.cdfive.common.util;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.binary.Hex;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -28,5 +32,14 @@ public class SHA256Util {
             log.error("encodeBySHA256 error,str={}", str, e);
             throw new RuntimeException("encodeBySHA256 error", e);
         }
+    }
+
+    public static String signBySHA256(String secret, Long timestamp) throws Exception {
+        String stringToSign = timestamp + "\n" + secret;
+        Mac mac = Mac.getInstance("HmacSHA256");
+        mac.init(new SecretKeySpec(secret.getBytes("UTF-8"), "HmacSHA256"));
+        byte[] signData = mac.doFinal(stringToSign.getBytes("UTF-8"));
+        String sign = URLEncoder.encode(new String(Base64.encodeBase64(signData)), "UTF-8");
+        return sign;
     }
 }
