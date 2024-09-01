1f15fdb9f38ee1d7e8e67ca30857c79fae4d32df
cdfive <cdfive@qq.com>
2024-09-01 21:53:43 +0800

    Improve JacksonUtil using jackson lib

--- cdfive-common/src/main/java/com/cdfive/common/util/JacksonUtil.java
+++ cdfive-common/src/main/java/com/cdfive/common/util/JacksonUtil.java
@@ -1,9 +1,16 @@
 package com.cdfive.common.util;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.guava.GuavaModule;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import java.io.IOException;
+import java.util.Map;
 
 /**
  * @author cdfive
@@ -13,8 +20,13 @@ public class JacksonUtil {
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     static {
-        OBJECT_MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        OBJECT_MAPPER.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        OBJECT_MAPPER
+                .registerModule(new Jdk8Module())
+                .registerModule(new JavaTimeModule())
+                .registerModule(new GuavaModule())
+//                .setSerializationInclusion(JsonInclude.Include.NON_NULL)
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
     }
 
     public static <T> T jsonToObj(String jsonStr, Class<T> clazz) {
@@ -25,21 +37,30 @@ public class JacksonUtil {
         try {
             return OBJECT_MAPPER.readValue(jsonStr, clazz);
         } catch (Exception e) {
-            throw new RuntimeException("JacksonUtil#jsonToObj error", e);
+            throw new RuntimeException("JacksonUtil#jsonToObj error,jsonStr=" + jsonStr, e);
         }
     }
 
     @SuppressWarnings({"rawtypes", "deprecation"})
-    public static <T> T jsonToObj(String content, Class<T> clazzItem, Class... classes) {
-        if (StringUtil.isBlank(content)) {
+    public static <T> T jsonToObj(String jsonStr, Class<T> clazzItem, Class... classes) {
+        if (StringUtil.isBlank(jsonStr)) {
             return null;
         }
 
         JavaType javaType = OBJECT_MAPPER.getTypeFactory().constructParametricType(clazzItem, classes);
         try {
-            return OBJECT_MAPPER.readValue(content, javaType);
+            return OBJECT_MAPPER.readValue(jsonStr, javaType);
         } catch (Exception e) {
-            throw new RuntimeException("JacksonUtil#jsonToObj error", e);
+            throw new RuntimeException("JacksonUtil#jsonToObj error,jsonStr=" + jsonStr, e);
+        }
+    }
+
+    public static <K, V> Map<K, V> jsonToMap(String jsonStr, Class<K> keyClass, Class<V> valueClass) {
+
+        try {
+            return OBJECT_MAPPER.readValue(jsonStr, OBJECT_MAPPER.getTypeFactory().constructParametricType(Map.class, new Class[]{keyClass, valueClass}));
+        } catch (IOException e) {
+            throw new RuntimeException("JacksonUtil#jsonToMap error,jsonStr=" + jsonStr, e);
         }
     }
 
