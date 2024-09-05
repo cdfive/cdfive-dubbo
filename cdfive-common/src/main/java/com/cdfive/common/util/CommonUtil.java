a6257fd0808db4a504b3fe13752dc336c4768ce4
cdfive <cdfive@qq.com>
2024-09-05 09:19:24 +0800

    Add two distinctByKey method in CommonUtil

--- cdfive-common/src/main/java/com/cdfive/common/util/CommonUtil.java
+++ cdfive-common/src/main/java/com/cdfive/common/util/CommonUtil.java
@@ -1,6 +1,12 @@
 package com.cdfive.common.util;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * @author cdfive
@@ -9,5 +15,15 @@ public class CommonUtil {
 
     public static String getTraceId() {
         return UUID.randomUUID().toString().replaceAll("-", "");
+    }
+
+    public static <T> Predicate<T> distinctByKey(Function<? super T, Object> keyExtractor) {
+        Map<Object, Boolean> map = new HashMap<>();
+        return t -> map.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
+    }
+
+    public static <T> Predicate<T> distinctByKey2(Function<? super T, ?> keyExtractor) {
+        Set<Object> seen = ConcurrentHashMap.newKeySet();
+        return t -> seen.add(keyExtractor.apply(t));
     }
 }
