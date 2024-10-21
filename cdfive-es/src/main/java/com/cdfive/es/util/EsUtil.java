bb6f443d9aeb4de6229e2b7c7b96043b77f665df
cdfive <cdfive@qq.com>
2024-10-21 14:32:36 +0800

    Add util method for get body string of HttpRequest

--- cdfive-es/src/main/java/com/cdfive/es/util/EsUtil.java
+++ cdfive-es/src/main/java/com/cdfive/es/util/EsUtil.java
@@ -1,6 +1,12 @@
 package com.cdfive.es.util;
 
 import com.cdfive.es.query.SearchQuery;
+import org.apache.http.HttpEntityEnclosingRequest;
+import org.apache.http.HttpRequest;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author cdfive
@@ -9,5 +15,15 @@ public class EsUtil {
 
     public static String genDsl(SearchQuery searchQuery) {
         return searchQuery.toSearchSourceBuilder().toString();
+    }
+
+    public static String getHttpRequestBody(HttpRequest request) {
+        try {
+            ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+            ((HttpEntityEnclosingRequest) request).getEntity().writeTo(outStream);
+            return new String(outStream.toByteArray(), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
