38d8fcb703a42de57e6f69ccd6cc49cb1b82f573
cdfive <cdfive@qq.com>
2024-10-28 15:46:00 +0800

    More test demo of using subList and clear of ArrayList

--- cdfive-learn/src/main/java/com/cdfive/learn/collection/SubListTest.java
+++ cdfive-learn/src/main/java/com/cdfive/learn/collection/SubListTest.java
@@ -1,5 +1,13 @@
 package com.cdfive.learn.collection;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -19,11 +27,38 @@ public class SubListTest {
             System.out.println(list.subList(3, 6));
         } catch (Exception e) {
             System.out.println(e instanceof IndexOutOfBoundsException);
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
 
         System.out.println(list.subList(3, 3));
         System.out.println(list.subList(3, 4));
         System.out.println(list.subList(3, 5));
+
+        System.out.println(StringUtils.center("分隔线", 50, "-"));
+
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("1001", "苹果"));
+        products.add(new Product("1002", "梨子"));
+        products.add(new Product("1003", "香蕉"));
+        System.out.println(products);
+
+        List<Product> subProducts = products.subList(2, products.size());
+        System.out.println(subProducts);
+        System.out.println(products);
+
+        subProducts.clear();
+        System.out.println(subProducts);
+        System.out.println(products);
+    }
+
+    @ToString
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Data
+    private static class Product {
+
+        private String code;
+
+        private String name;
     }
 }
